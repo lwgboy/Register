0852dcae990eef654404bdedb6df7b02024414e6
mengmeng <805711078@qq.com>
2018-04-24 11:51:23 +0800

    first commit

---
+++ src/com/itshop/utils/CommonsUtils.java
@@ -0,0 +1,9 @@
+package com.itshop.utils;
+
+import java.util.UUID;
+
+public class CommonsUtils {
+	public static String getUUID() {
+		return UUID.randomUUID().toString();
+	}
+}
---
+++ src/com/itshop/web/servlet/RegisterServlet.java
@@ -0,0 +1,96 @@
+package com.itshop.web.servlet;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.security.Provider.Service;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.Converter;
+
+import com.itshop.domain.User;
+import com.itshop.utils.CommonsUtils;
+import com.itshop.utils.MailUtils;
+import com.itshop.service.UserService;
+
+public class RegisterServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		//获得表单数据
+		Map<String, String[]> properties = request.getParameterMap();
+		
+		User user = new User();
+		try {
+			//自己制定一个类型转换器（将String转成Date）
+			ConvertUtils.register(new Converter() {
+				
+				@Override
+				public Object convert(Class clazz, Object value) {
+					//将string转成date
+					SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+					Date parse = null;
+					try {
+						parse = format.parse(value.toString());
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return parse;
+				}
+			}, Date.class);
+			//映射封装
+			BeanUtils.populate(user, properties);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+		//private String uid;
+		user.setUid(CommonsUtils.getUUID());
+		//private String telephone;
+		user.setTelephone(null);
+		//private int state;//是否激活
+		user.setState(0);
+		//private String code;
+		String activeCode = CommonsUtils.getUUID();
+		user.setCode(activeCode);
+		
+		//将user传给service层
+		UserService service = new UserService();
+		boolean isRegistersuccess = service.regist(user);
+		
+		if(isRegistersuccess) {
+			//跳转到注册成功页面
+			response.sendRedirect(request.getContextPath()+"/registerSuccess.jsp");
+			
+			//发送激活邮件
+			String emailMsg = "恭喜你注册成功，请点击下面的链接进行激活账户"
+					+ "<a href='http://localhost:8080/Shop/active?activeCode="+activeCode+"'>"
+					+"http://localhost:8080/Shop/active?activeCode="+activeCode+ "</a>";
+			try {
+				MailUtils.sendMail(user.getEmail(), emailMsg);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+		}else {
+			//registerFail.jsp
+			response.sendRedirect(request.getContextPath()+"/registerFail.jsp");
+		}
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
